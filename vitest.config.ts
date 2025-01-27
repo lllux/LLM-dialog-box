89f7b722068aee72353671669a288555ed51fe18
llllux <14513072+llllux@user.noreply.gitee.com>
2025-01-28 03:48:45 +0800

    init commit

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,13 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      
+      // 添加覆盖率配置
+      coverage: {
+        provider: 'v8', // 使用 V8 引擎生成覆盖率
+        reporter: ['text', 'json', 'html'], // 可选的报告格式
+        include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.js', 'src/**/*.vue'], // 包含文件
+      },
     },
   }),
 )
